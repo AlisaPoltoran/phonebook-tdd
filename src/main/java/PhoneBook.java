6f7861f3c300f7f0da835141a8ae8f886353d2ec
Alisa Poltoran <alisapoltoran@inbox.ru>
2022-12-13 07:42:14 +0300

    Stubs and tests for add method

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,12 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
+
+    Map<String, String> phoneBook = new TreeMap<>();
+
+    public int add(String name, String phoneNumber) {
+       return 0;
+    }
 }
+
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,51 @@
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class PhoneBookTest {
+
+    PhoneBook sut = new PhoneBook();
+
+    @BeforeEach
+    public void init() {
+        System.out.println("Test started");
+    }
+
+    @BeforeAll
+    public static void started() {
+        System.out.println("Tests started");
+    }
+
+    @AfterEach
+    public void finished() {
+        System.out.println("Test completed");
+    }
+
+    @AfterAll
+    public static void finishedAll() {
+        System.out.println("Tests completed");
+    }
+
+    @ParameterizedTest
+    @MethodSource("sourceForAdd")
+    public void testAdd(String name, String phoneNumber, int expected) {
+        sut.phoneBook.put("Pasha", "+7(812)888-79-12");
+        sut.phoneBook.put(name, phoneNumber);
+        int result = sut.phoneBook.size();
+        assertEquals(expected, result);
+    }
+    @MethodSource("sourceForAdd")
+    public static Stream<Arguments> sourceForAdd() {
+        return Stream.of(Arguments.of("Petya", "+7(812)888-77-66", 2),
+                Arguments.of("Masha", "+7(812)888-78-11", 2),
+                Arguments.of("Pasha", "+7(812)888-79-12",  1));
+    }
+
+
 }
